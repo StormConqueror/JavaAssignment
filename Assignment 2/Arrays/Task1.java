ad21c27d60b30f0f039f004e407bc629dde0a804
StormConqueror <nightlysky91@gmail.com>
2025-01-09 23:00:21 +0500

    Update Task1.java

--- Assignment 2/Arrays/Task1.java
+++ Assignment 2/Arrays/Task1.java
@@ -1,4 +1,7 @@
 package Arrays;
 public class Task1 {
-    
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
 }
