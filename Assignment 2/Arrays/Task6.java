9fe56fd45c1279b90d4e4b6010df699e7a6cf05e
StormConqueror <nightlysky91@gmail.com>
2025-01-09 23:03:18 +0500

    Create Task6.java

---
+++ Assignment 2/Arrays/Task6.java
@@ -0,0 +1,26 @@
+package Arrays;
+import java.util.Scanner;
+public class Task6 {
+    public static int[] array;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array:");
+        int n = sc.nextInt();
+
+        if (n > 0) {
+            array = new int[n];
+            int maxNum = Integer.MIN_VALUE;
+            for (int i = 0; i < n; i++) {
+                array[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (array[i] > maxNum) {
+                    maxNum = array[i];
+                }
+            }
+
+            System.out.println(maxNum);
+        }
+    }
+}
